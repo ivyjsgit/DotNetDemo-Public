db2fd0b9ccf8c8531aef76e0d3e64b2ee57e75c2
Innocent <nsabimanaii@hendrix.edu>
2020-09-14 13:35:02 -0500

    Set up an empty project template

---
+++ DotNetDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
